b3f70a8285f4d43186584dfcb7d92dfc925608d7
Taylor Todd <ttodd1031@gmail.com.com>
2023-04-14 22:56:42 -0500

    added hourly forecast

--- src/index.js
+++ src/index.js
@@ -1,25 +1,50 @@
-import { updateDisplay } from "./ui";
+import { updateCurrent, updateForecast } from "./ui";
+
 
 async function getWeather(searchedCity) {
-    const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=32f6de3ab826413d9e4145439231304&q=${searchedCity}`, {mode: 'cors'});
-    const data = await response.json();
-    console.log(data)
-    const city = data.location.name;
-    const tempF = data.current.temp_f + "°";
-    const tempC = data.current.temp_c;
-    const condition = data.current.condition.text;
-    const conditionIconSrc = data.current.condition.icon;
-    console.log(condition);
-    updateDisplay(city, tempF, tempC, condition, conditionIconSrc);
+    try {
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=32f6de3ab826413d9e4145439231304&q=${searchedCity}`, {mode: 'cors'});
+        const data = await response.json();
+        const city = data.location.name;
+        const tempF = data.current.temp_f + "°";
+        const tempC = data.current.temp_c;
+        const condition = data.current.condition.text;
+        const conditionIconSrc = data.current.condition.icon;
+        updateCurrent(city, tempF, tempC, condition, conditionIconSrc);
+    } catch(e) {
+        console.log(e);
+    }
+    
 }
 
-getWeather();
+async function getForecast(searchedCity) {
+    try {
+        const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=32f6de3ab826413d9e4145439231304&q=${searchedCity}&days=3&aqi=noalerts=no`, {mode: 'cors'});
+        const data = await response.json();
+        const forecast = data.forecast;
+        const currentTime = data.current.last_updated;
+        updateForecast(forecast, currentTime);
+    } catch(e) {
+        alert("Please enter a valid location")
+    }
+    
+}
+
+
+getWeather("London");
+getForecast("London");
 
 document.addEventListener("keypress", (e) => {
     if (e.code === "Enter") {
-        const city = citySearch.value;
-        console.log(e.code);
+        try {
+            const city = citySearch.value;
         getWeather(city);
+        getForecast(city);
+        document.getElementById("forecastContainer").innerHTML = "";
+        } catch(e) {
+            alert("Please enter a valid location");
+        }
+        
     }
 })
 
--- src/ui.js
+++ src/ui.js
@@ -1,9 +1,52 @@
 
-function updateDisplay(city, tempF, tempC, condition, conditionSrc,) {
+import {format} from 'date-fns';
+
+function updateCurrent(city, tempF, tempC, condition, conditionSrc, wind) {
     document.getElementById("cityName").innerHTML = city;
     document.getElementById("temperature").innerHTML = tempF;
     document.getElementById("condition").innerHTML = condition;
     document.getElementById("conditionLogo").setAttribute("src", conditionSrc);
 }
 
-export {updateDisplay}
+function updateForecast(forecast, currentTime) {
+
+    let currentTimeindex = parseInt(currentTime.slice(-5,-3));
+
+        // fix this, its currently an infinite loop
+    for (let i = currentTimeindex + 1; i <= currentTimeindex + 5; i++) {
+       let day = 0;
+
+        if (i >= 24) {
+            day = 1;
+            currentTimeindex = currentTimeindex - i;
+            i = 0;
+        }
+
+        const hourlyContainer = document.createElement("div");
+        hourlyContainer.setAttribute("class", "hourly");
+        hourlyContainer.setAttribute("id", i)
+        forecastContainer.appendChild(hourlyContainer);
+
+        const time = document.createElement("h4");
+        time.innerHTML = format(new Date(forecast.forecastday[day].hour[i].time), 'h a');
+        hourlyContainer.appendChild(time);
+
+        //condition image
+        conditionLogo = document.createElement("img");
+        conditionLogo.setAttribute("src", forecast.forecastday[day].hour[i].condition.icon);
+        hourlyContainer.appendChild(conditionLogo);
+
+        //condition
+        condition = document.createElement("center");
+        condition.innerHTML = forecast.forecastday[day].hour[i].condition.text;
+        hourlyContainer.appendChild(condition);
+
+        //temperature
+        temperature = document.createElement("div")
+        temperature.setAttribute("class", "forecastTemp")
+        temperature.innerHTML = forecast.forecastday[day].hour[i].temp_f + "°";
+        hourlyContainer.appendChild(temperature)
+    }
+}
+
+export {updateCurrent, updateForecast}
